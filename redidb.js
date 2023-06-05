349b19c3807367e1700675cd9b654f2de0fe3c50
s1mofrek <49340895+s1mofrek@users.noreply.github.com>
2023-06-05 04:42:20 +0300

    v4.1.1
    + Bug fixing

--- redidb.js
+++ redidb.js
@@ -8,14 +8,15 @@ module.exports = class redidb extends EventEmitter {
 		this.setMaxListeners(25000);
 
 		this.link = argv.websocket ? `${argv.useSSL ? 'wss' : 'ws'}://${argv.ip}${argv.port ? ':' + argv.port : ''}/ws?login=${argv.login}&password=${argv.password}` : `${argv.useSSL ? 'https' : 'http'}://${argv.ip}${argv.port ? ':' + argv.port : ''}`;
+		this.listLink = `${argv.useSSL ? 'https' : 'http'}://${argv.ip}${argv.port ? ':' + argv.port : ''}`;
 		this.method = argv.websocket ? 'WS' : 'HTTP';
 
-		if (this.method == 'HTTP')
-			this.authorization = {
-				login: argv.login,
-				password: argv.password,
-			};
-		else {
+		this.authorization = {
+			login: argv.login,
+			password: argv.password,
+		};
+
+		if (this.method == 'WS') {
 			this.ws = new WebSocket(this.link);
 			this.ws.setMaxListeners(25000);
 
