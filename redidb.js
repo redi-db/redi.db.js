6d8eebe1099831699116a73edb20f04dc4e58d7e
s1mofrek <egor.chehovskiy@ukr.net>
2023-07-29 13:10:02 +0300

    v4.1.6
    + Optimized operation of the client web socket

--- redidb.js
+++ redidb.js
@@ -1,12 +1,12 @@
 const Collection = require('./lib/Collection');
 const EventEmitter = require('events');
+const clients = require('./lib/clients');
 const WebSocket = require('ws');
 const PWS = require('pws');
 
-module.exports = class redidb extends EventEmitter {
+module.exports = class RediDB extends EventEmitter {
 	constructor(argv) {
 		super();
-		this.setMaxListeners(25000);
 
 		this.link = argv.websocket ? `${argv.useSSL ? 'wss' : 'ws'}://${argv.ip}${argv.port ? ':' + argv.port : ''}/ws?login=${argv.login}&password=${argv.password}` : `${argv.useSSL ? 'https' : 'http'}://${argv.ip}${argv.port ? ':' + argv.port : ''}`;
 		this.listLink = `${argv.useSSL ? 'https' : 'http'}://${argv.ip}${argv.port ? ':' + argv.port : ''}`;
@@ -18,23 +18,22 @@ module.exports = class redidb extends EventEmitter {
 		};
 
 		if (this.method == 'WS') {
-			this.ws = new PWS(this.link, WebSocket);
-
-			this.disconnect = this.ws.close;
-			this.connect = this.ws.connect;
+			this.clientID = clients.add(new PWS(this.link, WebSocket));
+			this.disconnect = clients.get(this.clientID).close;
+			this.connect = clients.get(this.clientID).connect;
 			this.connected = false;
 
-			this.ws.firstConnect = false;
-			this.ws.on('open', () => {
-				this.ws.firstConnect = true;
+			clients.get(this.clientID).firstConnect = false;
+			clients.get(this.clientID).on('open', () => {
+				clients.get(this.clientID).firstConnect = true;
 				this.connected = true;
 
 				this.emit('connected');
 			});
 
-			this.ws.on('close', () => {
+			clients.get(this.clientID).on('close', () => {
 				this.connected = false;
-				if (this.ws.firstConnect) this.emit('disconnect');
+				if (clients.get(this.clientID).firstConnect) this.emit('disconnect');
 			});
 		}
 	}
@@ -43,12 +42,16 @@ module.exports = class redidb extends EventEmitter {
 		if (typeof options.retryAfter != 'number') options.retryAfter = 30;
 		if (typeof options.maxRetries != 'number') options.maxRetries = 3;
 
-		if (options.retryAfter < 0) throw new Error('<retryAfter> must be >= 0')
-		if (options.maxRetries < 0) throw new Error('<maxRetries> must be >= 0')
+		if (options.retryAfter < 0) throw new Error('<retryAfter> must be >= 0');
+		if (options.maxRetries < 0) throw new Error('<maxRetries> must be >= 0');
 
-		return new Collection(this, {
-			database,
-			collection,
-		}, options);
+		return new Collection(
+			this,
+			{
+				database,
+				collection,
+			},
+			options
+		);
 	}
 };
